07c54f1eab4d32e431a92ab5d304d4246a4b3c89
Kyle Kwong <kyle@lob.com>
2018-07-19 13:24:09 -0700

    refactor tests

--- logger_global_test.go
+++ logger_global_test.go
@@ -1,79 +1,12 @@
 package logger
 
 import (
-	"bytes"
-	"fmt"
-	"io"
-	"os"
-	"strings"
 	"testing"
 )
 
-func testGlobalLogger(t *testing.T, infoLevel string, infoMsg string) {
-	origStdout := os.Stdout
-	defer func() { os.Stdout = origStdout }()
-	r, w, err := os.Pipe()
-	if err != nil {
-		t.Fatalf("unexpected error when creating a pipe: %s", err)
-	}
-	os.Stdout = w
-
-	defaultLogger = New()
-	rootData := map[string]interface{}{"r1": "test", "r2": "moreTest"}
-	Root(rootData)
-
-	d1, d2, d3, d4 :=
-		map[string]interface{}{"1": "1"},
-		map[string]interface{}{"2": 2},
-		map[string]interface{}{"3": []int{3, 4, 5}},
-		map[string]interface{}{"4": map[string]interface{}{"5": 6.5}}
-
-	outC := make(chan string)
-	go func() {
-		var buf bytes.Buffer
-		io.Copy(&buf, r)
-		outC <- buf.String()
-	}()
-
-	switch infoLevel {
-	case "error":
-		Error(infoMsg, d1, d2, d3, d4)
-	case "warn":
-		Warn(infoMsg, d1, d2, d3, d4)
-	case "debug":
-		Debug(infoMsg, d1, d2, d3, d4)
-	case "fatal":
-		Fatal(infoMsg, d1, d2, d3, d4)
-	case "info":
-		Info(infoMsg, d1, d2, d3, d4)
-	}
-
-	if err = w.Close(); err != nil {
-		t.Fatalf("unexpected error closing write pipe: %s", err)
-	}
-
-	logLine := <-outC
-
-	if !strings.Contains(logLine, fmt.Sprintf(`"level":"%s"`, infoLevel)) {
-		t.Error("Log level is incorrect")
-	} else if !strings.Contains(logLine, `"host":`) {
-		t.Error("Host is missing")
-	} else if !strings.Contains(logLine, `"release":`) {
-		t.Error("Release is missing")
-	} else if !strings.Contains(logLine, `"nanoseconds":`) {
-		t.Error("Nanoseconds is missing")
-	} else if !strings.Contains(logLine, `"timestamp":`) {
-		t.Error("Timestamp is missing")
-	} else if !strings.Contains(logLine, `"r1":"test"`) || !strings.Contains(logLine, `"r2":"moreTest"`) {
-		t.Error("Root data is incorrect")
-	} else if !strings.Contains(logLine, `"data":{"1":"1","2":2,"3":[3,4,5],"4":{"5":6.5}}`) {
-		t.Error("Data is incorrect")
-	}
-}
-
 func TestGlobalLogs(t *testing.T) {
-	testGlobalLogger(t, "info", "info test")
-	testGlobalLogger(t, "error", "error test")
-	testGlobalLogger(t, "debug", "debug test")
-	testGlobalLogger(t, "warn", "warn test")
+	testLogger(t, "info", "info test", true)
+	testLogger(t, "error", "error test", true)
+	testLogger(t, "debug", "debug test", true)
+	testLogger(t, "warn", "warn test", true)
 }
--- logger_test.go
+++ logger_test.go
@@ -9,20 +9,31 @@ import (
 	"testing"
 )
 
-func testLog(t *testing.T, infoLevel string, infoMsg string) {
-	id := "testId"
-
-	data, rootData := map[string]interface{}{"data": "test"}, map[string]interface{}{"r1": "test", "r2": "moreTest"}
+func testLogger(t *testing.T, infoLevel string, infoMsg string, global bool) {
+	var id string
+	var data map[string]interface{}
+	var log Logger
+	rootData := map[string]interface{}{"r1": "test", "r2": "moreTest"}
 
 	origStdout := os.Stdout
-	defer func() { os.Stdout = origStdout }()
+	defer func() {
+		os.Stdout = origStdout
+		defaultLogger = New()
+	}()
 	r, w, err := os.Pipe()
 	if err != nil {
 		t.Fatalf("unexpected error when creating a pipe: %s", err)
 	}
 	os.Stdout = w
 
-	log := New().ID(id).Data(data).Root(rootData)
+	if global {
+		defaultLogger = New()
+		Root(rootData)
+	} else {
+		id = "testId"
+		data = map[string]interface{}{"data": "test"}
+		log = New().ID(id).Data(data).Root(rootData)
+	}
 
 	d1, d2, d3, d4 :=
 		map[string]interface{}{"1": "1"},
@@ -37,17 +48,32 @@ func testLog(t *testing.T, infoLevel string, infoMsg string) {
 		outC <- buf.String()
 	}()
 
-	switch infoLevel {
-	case "error":
-		log.Error(infoMsg, d1, d2, d3, d4)
-	case "warn":
-		log.Warn(infoMsg, d1, d2, d3, d4)
-	case "debug":
-		log.Debug(infoMsg, d1, d2, d3, d4)
-	case "fatal":
-		log.Fatal(infoMsg, d1, d2, d3, d4)
-	default:
-		log.Info(infoMsg, d1, d2, d3, d4)
+	if global {
+		switch infoLevel {
+		case "error":
+			Error(infoMsg, d1, d2, d3, d4)
+		case "warn":
+			Warn(infoMsg, d1, d2, d3, d4)
+		case "debug":
+			Debug(infoMsg, d1, d2, d3, d4)
+		case "fatal":
+			Fatal(infoMsg, d1, d2, d3, d4)
+		case "info":
+			Info(infoMsg, d1, d2, d3, d4)
+		}
+	} else {
+		switch infoLevel {
+		case "error":
+			log.Error(infoMsg, d1, d2, d3, d4)
+		case "warn":
+			log.Warn(infoMsg, d1, d2, d3, d4)
+		case "debug":
+			log.Debug(infoMsg, d1, d2, d3, d4)
+		case "fatal":
+			log.Fatal(infoMsg, d1, d2, d3, d4)
+		default:
+			log.Info(infoMsg, d1, d2, d3, d4)
+		}
 	}
 
 	if err = w.Close(); err != nil {
@@ -55,33 +81,51 @@ func testLog(t *testing.T, infoLevel string, infoMsg string) {
 	}
 
 	logLine := <-outC
-
-	if !strings.Contains(logLine, fmt.Sprintf(`"id":"%s"`, id)) {
-		t.Error("ID is incorrect")
-	} else if !strings.Contains(logLine, fmt.Sprintf(`"level":"%s"`, infoLevel)) {
-		t.Error("Log level is incorrect")
-	} else if !strings.Contains(logLine, `"host":`) {
-		t.Error("Host is missing")
-	} else if !strings.Contains(logLine, `"release":`) {
-		t.Error("Release is missing")
-	} else if !strings.Contains(logLine, `"nanoseconds":`) {
-		t.Error("Nanoseconds is missing")
-	} else if !strings.Contains(logLine, `"timestamp":`) {
-		t.Error("Timestamp is missing")
-	} else if !strings.Contains(logLine, `"r1":"test"`) || !strings.Contains(logLine, `"r2":"moreTest"`) {
-		t.Error("Root data is incorrect")
-	} else if !strings.Contains(logLine, `"data":{"data":"test","1":"1","2":2,"3":[3,4,5],"4":{"5":6.5}}`) {
-		t.Error("Data is incorrect")
+	if global {
+		if !strings.Contains(logLine, fmt.Sprintf(`"level":"%s"`, infoLevel)) {
+			t.Error("Log level is incorrect")
+		} else if !strings.Contains(logLine, `"host":`) {
+			t.Error("Host is missing")
+		} else if !strings.Contains(logLine, `"release":`) {
+			t.Error("Release is missing")
+		} else if !strings.Contains(logLine, `"nanoseconds":`) {
+			t.Error("Nanoseconds is missing")
+		} else if !strings.Contains(logLine, `"timestamp":`) {
+			t.Error("Timestamp is missing")
+		} else if !strings.Contains(logLine, `"r1":"test"`) || !strings.Contains(logLine, `"r2":"moreTest"`) {
+			t.Error("Root data is incorrect")
+		} else if !strings.Contains(logLine, `"data":{"1":"1","2":2,"3":[3,4,5],"4":{"5":6.5}}`) {
+			t.Error("Data is incorrect")
+		}
+	} else {
+		if !strings.Contains(logLine, fmt.Sprintf(`"id":"%s"`, id)) {
+			t.Error("ID is incorrect")
+		} else if !strings.Contains(logLine, fmt.Sprintf(`"level":"%s"`, infoLevel)) {
+			t.Error("Log level is incorrect")
+		} else if !strings.Contains(logLine, `"host":`) {
+			t.Error("Host is missing")
+		} else if !strings.Contains(logLine, `"release":`) {
+			t.Error("Release is missing")
+		} else if !strings.Contains(logLine, `"nanoseconds":`) {
+			t.Error("Nanoseconds is missing")
+		} else if !strings.Contains(logLine, `"timestamp":`) {
+			t.Error("Timestamp is missing")
+		} else if !strings.Contains(logLine, `"r1":"test"`) || !strings.Contains(logLine, `"r2":"moreTest"`) {
+			t.Error("Root data is incorrect")
+		} else if !strings.Contains(logLine, `"data":{"data":"test","1":"1","2":2,"3":[3,4,5],"4":{"5":6.5}}`) {
+			t.Error("Data is incorrect")
+		}
 	}
 }
+
 func TestMain(m *testing.M) {
 	code := m.Run()
 	os.Exit(code)
 }
 
 func TestLogs(t *testing.T) {
-	testLog(t, "info", "info test")
-	testLog(t, "error", "error test")
-	testLog(t, "debug", "debug test")
-	testLog(t, "warn", "warn test")
+	testLogger(t, "info", "info test", false)
+	testLogger(t, "error", "error test", false)
+	testLogger(t, "debug", "debug test", false)
+	testLogger(t, "warn", "warn test", false)
 }
